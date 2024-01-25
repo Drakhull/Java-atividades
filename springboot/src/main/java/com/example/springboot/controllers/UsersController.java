9eab20c2bc1973017e466d4c817eb393b2d5b19c
iurig <iurig@Daedra>
2024-01-25 20:24:41 -0300

    Método Get

--- springboot/src/main/java/com/example/springboot/controllers/UsersController.java
+++ springboot/src/main/java/com/example/springboot/controllers/UsersController.java
@@ -1,9 +1,15 @@
 package com.example.springboot.controllers;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,5 +31,19 @@ public class UsersController {
 		var usersModel = new UsersModel();
 		BeanUtils.copyProperties(usersRecordDto, usersModel);
 		return ResponseEntity.status(HttpStatus.CREATED).body(usersRepository.save(usersModel));
+	}
+	
+	@GetMapping("/users")
+	public ResponseEntity<List<UsersModel>> getAllUsers(){
+		return ResponseEntity.status(HttpStatus.OK).body(usersRepository.findAll());
+	}
+	
+	@GetMapping("/users/{id}")
+	public ResponseEntity<Object> getOneUser(@PathVariable(value="id") UUID id){
+		Optional<UsersModel> user0= usersRepository.findById(id);
+		if(user0.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado.");
+		}
+		return ResponseEntity.status(HttpStatus.OK).body(user0.get());
 	}
 }
